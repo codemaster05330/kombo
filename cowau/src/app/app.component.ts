a86f8d67a84993f67c414975a170406a4b979f9f
codemaster05330 <thomasj0830@gmail.com>
2018-04-12 14:31:41 +0200

    style headlines

--- cowau/src/app/app.component.ts
+++ cowau/src/app/app.component.ts
@@ -16,7 +16,7 @@ import { ServerPage } from '../pages/server/server';
 })
 
 export class MyApp {
-	rootPage:any = FlipitPage;
+	rootPage:any = EmojiPage;
 	// rootPage:any = ServerPage;
 
 	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
