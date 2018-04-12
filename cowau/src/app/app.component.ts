039a318b0e3a313b0f15a0239c951fb542ccebd3
codemaster05330 <thomasj0830@gmail.com>
2018-04-12 14:48:15 +0200

    Edit FliptIt Popover

--- cowau/src/app/app.component.ts
+++ cowau/src/app/app.component.ts
@@ -16,7 +16,8 @@ import { ServerPage } from '../pages/server/server';
 })
 
 export class MyApp {
-	rootPage:any = EmojiPage;
+	rootPage:any = FlipitPage;
+	// rootPage:any = EmojiPage;
 	// rootPage:any = ServerPage;
 
 	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
