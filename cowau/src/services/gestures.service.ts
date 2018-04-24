2284e739faafec8ef18a22d51852a087d42d588c
codemaster05330 <thomasj0830@gmail.com>
2018-04-24 21:36:37 +0200

    better flipit gesture

--- cowau/src/services/gestures.service.ts
+++ cowau/src/services/gestures.service.ts
@@ -11,8 +11,20 @@ export class GesturesService {
 	devMotionSubscription:any;
 	gyroSubscription:any;
 
+	acMedianXarray:Array<number> = new Array<number>();
+	acMedianYarray:Array<number> = new Array<number>();
+	acMedianZarray:Array<number> = new Array<number>();
+
+	acMedianX:number = 0;
+	acMedianY:number = 2;
+	acMedianZ:number = 9.81;
+
+	countAccelerationData:number = 0;
+
 	flipArray:Array<any> = new Array<any>();
 	throwArray:Array<any> = new Array<any>();
+
+	stillStandingTreshold:number = 1.5;
 	
 	constructor(public devMotion:DeviceMotion, public gyro:Gyroscope, public platform:Platform, public events:Events) {}
 
@@ -37,37 +49,70 @@ export class GesturesService {
 		let flipUp:boolean = false;
 		let flipGyroDown:boolean = false;
 		let flipGyroUp:boolean = false;
-
+		let checkFlip:boolean = true;
 
 		if(this.flipArray.length == arraySize) {
+			this.acMedianX = medianOfArray(this.acMedianXarray);
+			this.acMedianY = medianOfArray(this.acMedianYarray);
+			this.acMedianZ = medianOfArray(this.acMedianZarray);
+
+			// console.log(this.acMedianX, this.acMedianY, this.acMedianZ);
+
+			this.acMedianXarray = this.acMedianXarray.slice(1);
+			this.acMedianYarray = this.acMedianYarray.slice(1);
+			this.acMedianZarray = this.acMedianZarray.slice(1);
+			
 			this.flipArray = this.flipArray.slice(1);
+
+			this.countAccelerationData = 0;
 		}
 
+		this.acMedianXarray[this.countAccelerationData] = acceleration.x;
+		this.acMedianYarray[this.countAccelerationData] = acceleration.y;
+		this.acMedianZarray[this.countAccelerationData] = acceleration.z;
+
+		// this.flipArray[this.countAccelerationData] = {devmo: acceleration, gyro: ''};
+
+		// console.log('MEDIAN Z: ' + this.acMedianZ);
+
 		this.gyro.getCurrent().then((orientation:GyroscopeOrientation) => {
-			this.flipArray.push({devmo_z: acceleration.z, gyro_y: orientation.y});
+			this.flipArray[this.countAccelerationData] = {devmo: acceleration, gyro: orientation};
+			this.countAccelerationData++;
 
-			this.flipArray.forEach((value, index) => {
-				if(value.devmo_z < -8) {
-					flipDown = true;
-				}
+			if(acceleration.z < (this.acMedianZ - this.stillStandingTreshold) || acceleration.z > (this.acMedianZ + this.stillStandingTreshold)) {
 
-				if(value.devmo_z > 8) {
-					flipUp = true;
-				}
+				this.flipArray.forEach((value, index) => {
+					//check acceleration state
+					if(value.devmo.z < 2) {
+						flipDown = true;
+					}
+					if(value.devmo.z > 8) {
+						flipUp = true;
+					}
 
-				if(value.gyro_y > 4) {
-					flipGyroUp = true;
-				}
-				if (value.gyro_y < -4){
-					flipGyroDown = true;
-				}
 
-				if(flipDown && flipUp && flipGyroUp && flipGyroDown) {
-					flipDown = flipUp = flipGyroUp = flipGyroDown = false;
-					this.sendEvent('flipped', value);
-					this.flipArray = new Array<number>();
-				}
-			});
+					//check gyro state
+					if(value.gyro.y > 3) {
+						flipGyroUp = true;
+					}
+					if(value.gyro.y < -3){
+						flipGyroDown = true;
+					}
+					
+					//controll check
+					if(value.devmo.y > (this.acMedianY + this.stillStandingTreshold) || value.devmo.y < (this.acMedianY - this.stillStandingTreshold)) {
+						checkFlip = false;
+					}
+
+					if(flipDown && flipUp && flipGyroUp && flipGyroDown && checkFlip) {
+						flipDown = flipUp = flipGyroUp = flipGyroDown = false;
+						checkFlip = true;
+						this.sendEvent('flipped', value);
+						this.flipArray = new Array<number>();
+						this.countAccelerationData = 0;
+					}
+				});
+			}
 		});
 	}
 
@@ -140,6 +185,23 @@ export class GesturesService {
 	private sendEvent(name:string, value:any) {
 		this.events.publish(name, value);
 	}
+}
+
+function medianOfArray(values:Array<number>):number {
+	let median:number;
+	let sortedArray:Array<number>;
+
+	sortedArray = values.sort();
+
+	//even or odd amount of values
+	if(sortedArray.length % 2 == 0) {
+		let halfAmount = sortedArray.length / 2;
+		median = 0.5 * (sortedArray[halfAmount] + sortedArray[(halfAmount + 1)]);
+	} else {
+		median = sortedArray[((sortedArray.length + 1) / 2)];
+	}
+
+	return median;
 }
 
 function roundFloat(num:number, precision:number) {
