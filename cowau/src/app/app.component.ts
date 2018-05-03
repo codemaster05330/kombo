e2f7768f13d9fb0b52b0ff4f543f8c78a5d5e5ff
codemaster05330 <thomasj0830@gmail.com>
2018-05-03 17:01:28 +0200

    Audio Update

--- cowau/src/app/app.component.ts
+++ cowau/src/app/app.component.ts
@@ -16,7 +16,7 @@ import { ServerPage } from '../pages/server/server';
 })
 
 export class MyApp {
-	rootPage:any = FlipitPage;
+	rootPage:any = VisualPage;
 	// rootPage:any = ServerPage;
 
 	constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
--- cowau/src/providers/audio/audio.ts
+++ cowau/src/providers/audio/audio.ts
@@ -1,33 +1,69 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Http, ResponseContentType } from '@angular/http';
-
-declare var AudioContext;
-declare var webkitAudioContext;
-
-@Injectable()
-export class AudioProvider {
-
-    // Object for handyling user feedback with Ionic's LoadingController API Methods
-    private _PRELOADER              : any;
-    // Object for decoded audio data
-    private _TRACK                  : any;
-    // Object for decoded audioo data
-    private _AUDIO                  : any;
-    // Object for handling audio buffer data
-    private _SOURCE                 : any;
-    // Object for handling audio context
-    private _CONTEXT                : any;
-    // Object for handling audio volume changes
-    private _GAIN                   : any = null;
-
-
-    constructor(public http: HttpClient) {
-        console.log('Hello AudioProvider Provider');
-
-    }
-
-    loadSound(track : string) : void {
-    }
-
-}
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Http, ResponseContentType } from '@angular/http';
+//
+// declare var AudioContext;
+//
+// @Injectable()
+// export class AudioProvider {
+//
+//     private _TRACK                  : any = null;
+//     private _AUDIO                  : any;
+//     private _SOURCE                 : any;
+//     private _CONTEXT                : any = new (AudioContext)();
+//     private _GAIN                   : any = null;
+//
+//     constructor(public http     : HttpClient,
+//                 private _LOADER : LoadingController) {
+//     }
+//
+//     loadSound(track : string) : void {
+//         this.http.get(track, { responseType: ResponseContentType.ArrayBuffer })
+//         .map(res => res.arrayBuffer())
+//         .subscribe((arrayBufferContent : any) =>
+//         {
+//             this.setUpAudio(arrayBufferContent);
+//         });
+//     }
+//
+//     setUpAudio(bufferedContent : any) : void {
+//         this._CONTEXT.decodeAudioData(bufferedContent, (buffer : any) =>
+//         {
+//             this._AUDIO         = buffer;
+//             this._TRACK         = this._AUDIO;
+//             this.playSound(this._TRACK);
+//         });
+//     }
+//
+//     playSound(track : any) : void {
+//         if(!this._CONTEXT.createGain) {
+//             this._CONTEXT.createGain = this._CONTEXT.createGainNode;
+//         }
+//         this._GAIN                      = this._CONTEXT.createGain;
+//         this._SOURCE                    = this._CONTEXT.createBufferSource();
+//         this._SOURCE.buffer             = track;
+//         this._SOURCE.connect(this._GAIN);
+//         this._GAIN.connect(this._CONTEXT.destination);
+//
+//         this._SOURCE.start(0);
+//     }
+//
+//     stopSound() : void {
+//         if( !this._SOURCE.stop) {
+//             this._SOURCE.stop = this._SOURCE.noteOff;
+//         }
+//         this._SOURCE.stop(0);
+//     }
+//
+//     changeVolume(volume : any) : void {
+//         let percentile : number = parseInt(volume.value) / parseInt(volume.max);
+//         this._GAIN.gain.value = percentile * percentile;
+//     }
+//
+// }
+//
+// // let c = new AudioContext();
+// // let osc = c.createOscillator();
+// // osc.frequency.value = 440;
+// // osc.connect(c.destination);
+// // osc.start(0);
