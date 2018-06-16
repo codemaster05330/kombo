7ffab87fe99069a571c5dec20f31ec5a9dab1195
codemaster05330 <thomasj0830@gmail.com>
2018-06-16 14:30:49 +0200

    Load Page now loads, instead of app-components.

--- cowau/src/app/app.component.ts
+++ cowau/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ServerConnectionService } from '../services/server-connection.service';
-import { AudioBufferLoader } from 'waves-loaders';
-import { MetricSync } from '../services/metric-sync.service';
 
 // NOTE: Auskommentiert da es sonnst unused ist.
 import { Variables } from '../classes/variables';
@@ -17,8 +14,6 @@ import { EditPage } from '../pages/edit/edit';
 import { Socket } from 'ng-socket-io';
 
 
-import * as soundsData from '../assets/sounds/sounds.json';
-
 @Component({
 	templateUrl: 'app.html'
 })
@@ -29,34 +24,11 @@ export class MyApp {
 
 	@ViewChild('navCtrl') navCtrl:NavController;
 
-	constructor(public server:ServerConnectionService, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, socket:Socket,
-		globalVars:Variables, private metricSync:MetricSync) {
+	constructor(platform: Platform, statusBar: StatusBar, socket:Socket, globalVars:Variables) {
 		platform.ready().then(() => {
 			// Okay, so the platform is ready and our plugins are available.
 			// Here you can do any higher level native things you might need.
 			statusBar.hide();
-
-			this.server.initServerConnection().then(() => {
-				globalVars.audioBufferLoader = new AudioBufferLoader();
-				var soundsArrayString = [];
-
-				soundsData[0].forEach(soundsData => {
-					soundsArrayString = soundsArrayString.concat(soundsData.path);   		// New "big" Sound Array
-					globalVars.soundLengths = globalVars.soundLengths.concat(soundsData.length);
-				});
-
-				globalVars.audioBufferLoader.load(soundsArrayString)                                          // Load every Sound
-				.then((buffers) => {
-					const sendFunction = (cmd, ...args) => socket.emit(cmd, ...args);
-					const receiveFunction = (cmd, args) => socket.on(cmd, args);
-					globalVars.buffers = buffers;
-					metricSync.start(sendFunction, receiveFunction).then(() => {
-						console.log("metricSyncStarted");
-						this.navCtrl.setRoot(EditPage);
-					});
-				});
-
-			});
 
 			platform.pause.subscribe(() => {
 				if(globalVars.emojiID != null) {
--- cowau/src/pages/load/load.ts
+++ cowau/src/pages/load/load.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 import { NavController, NavParams, Events } from 'ionic-angular';
 import { MetricSync } from '../../services/metric-sync.service';
 import { Socket } from 'ng-socket-io';
 import { GesturesService } from '../../services/gestures.service';
+import { ServerConnectionService } from '../../services/server-connection.service';
+import { AudioBufferLoader } from 'waves-loaders';
 
 // Import the pages, that are needed for this page
-import { EmojiPage } from '../emoji/emoji';
+// import { EmojiPage } from '../emoji/emoji';
+import { EditPage } from '../edit/edit';
 
 // Import every classes
 import { Variables } from '../../classes/variables';
 import { GestureType } from '../../classes/gesture-type';
 import { SoundWave } from '../../classes/sound-wave';
+
+
+import * as soundsData from '../../assets/sounds/sounds.json';
 
 @Component({
 	selector: 'page-load',
@@ -18,9 +24,40 @@ import { SoundWave } from '../../classes/sound-wave';
 })
 
 export class LoadingPage {
+	text : string = "click here";
+	isLoading: boolean = false;
 
-
-    constructor() {
+    constructor(private navCtrl: NavController, private socket:Socket, private globalVars:Variables, 
+    	private metricSync:MetricSync, private server: ServerConnectionService) {
         console.log("Loading Page");
+    }
+
+    onClick(){
+    	if(!this.isLoading){
+    		this.isLoading = true;
+    		this.text = "Loading...";
+
+    		this.server.initServerConnection().then(() => {
+				this.globalVars.audioBufferLoader = new AudioBufferLoader();
+				var soundsArrayString = [];
+
+				soundsData[0].forEach(soundsData => {
+					soundsArrayString = soundsArrayString.concat(soundsData.path);   		// New "big" Sound Array
+					this.globalVars.soundLengths = this.globalVars.soundLengths.concat(soundsData.length);
+				});
+
+				this.globalVars.audioBufferLoader.load(soundsArrayString)                                          // Load every Sound
+				.then((buffers) => {
+					const sendFunction = (cmd, ...args) => this.socket.emit(cmd, ...args);
+					const receiveFunction = (cmd, args) => this.socket.on(cmd, args);
+					this.globalVars.buffers = buffers;
+					this.metricSync.start(sendFunction, receiveFunction).then(() => {
+						console.log("metricSyncStarted");
+						this.navCtrl.setRoot(EditPage);
+					});
+				});
+
+			});
+    	}
     }
 }
