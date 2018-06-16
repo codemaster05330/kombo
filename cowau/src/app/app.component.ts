8022d02e4b9775adecfc903e7fce48feaee1e3a2
codemaster05330 <thomasj0830@gmail.com>
2018-06-16 14:58:26 +0200

    flipit clean code

--- cowau/src/app/app.component.ts
+++ cowau/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
 import { Platform, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
