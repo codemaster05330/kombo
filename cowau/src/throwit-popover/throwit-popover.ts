4f46726399266f444c8ba4bdc9536e4222d4cd3a
codemaster05330 <thomasj0830@gmail.com>
2018-06-26 18:04:01 +0200

    fixer fix

--- cowau/src/throwit-popover/throwit-popover.ts
+++ cowau/src/throwit-popover/throwit-popover.ts
@@ -7,15 +7,8 @@ import { PopoverController, ViewController } from 'ionic-angular';
 })
 
 export class ThrowItPopoverPage {
-	videoSource:string;
-	@ViewChild('videoPlayer') videoplayer: any;
 	
 	constructor(public popoverCtrl: PopoverController, private viewCtrl:ViewController) {}
-
-	public playVid() {
-		this.videoplayer.nativeElement.play();
-		this.videoplayer.nativeElement.loop = true;
-	}
 
 	closePopover() {
 		this.viewCtrl.dismiss();
